10c978584f0d9cc92b91a87b167e8036cd2c4648
Jevgeni Kalinin <jevgeni.kalinin@gmail.com>
2024-12-15 11:28:59 +0200

    added shop item list

--- dragons-frontend/src/components/Shop.tsx
+++ dragons-frontend/src/components/Shop.tsx
@@ -1,5 +1,38 @@
+import {ShopItem} from "../etc/types.ts";
+import {useEffect, useState} from "react";
+import {useAppSelector} from "../store/store.ts";
+
 export function Shop() {
+
+  const gameId = useAppSelector(state => state.gameState.game.gameId);
+  const [shopItems, setShopItems] = useState<ShopItem[]>([]);
+
+  function fetchShopItems() {
+    fetch(`https://dragonsofmugloar.com/api/v2/${gameId}/shop`)
+    .then(response => response.json())
+    .then(data => setShopItems(data as ShopItem[]))
+  }
+
+  useEffect(() => {
+    fetchShopItems();
+  }, []);
+
   return (
-    <div>shop</div>  
+      <section>
+        <div>shop <button onClick={fetchShopItems}>refresh</button></div>
+        {shopItems.map(shopItem => <ShopProduct key={shopItem.id} shopItem={shopItem}/>)}
+      </section>
+  );
+}
+
+function ShopProduct({shopItem}: { shopItem: ShopItem }) {
+  return (
+      <details>
+        <summary>{shopItem.name}</summary>
+        <div>price: {shopItem.cost}</div>
+        <div>
+          <button>buy</button>
+        </div>
+      </details>
   );
 }
