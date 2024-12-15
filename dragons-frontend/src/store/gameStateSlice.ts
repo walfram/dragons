18aa21f8d276d47b9318a64977d35addd6950e3f
Jevgeni Kalinin <jevgeni.kalinin@gmail.com>
2024-12-15 11:29:00 +0200

    implemented accept quest

--- dragons-frontend/src/store/gameStateSlice.ts
+++ dragons-frontend/src/store/gameStateSlice.ts
@@ -1,5 +1,6 @@
 import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {StartGameResponse} from "../etc/types.ts";
+import {QuestResult, StartGameResponse} from "../etc/types.ts";
+import {RootState} from "./store.ts";
 
 type GameState = {
   started: boolean;
@@ -22,25 +23,50 @@ const initialGameState: GameState = {
 export const gameStateSlice = createSlice({
   name: 'game-state-slice',
   initialState: initialGameState,
-  reducers: {
-  },
+  reducers: {},
   extraReducers: builder => {
     builder.addCase(startNewGame.fulfilled, (state, action: PayloadAction<StartGameResponse>) => {
       state.started = true;
       state.game = action.payload;
     });
     builder.addCase(startNewGame.pending, (_state) => {
+
+    });
+    builder.addCase(startNewGame.rejected, (state, action) => {
+      state.started = false;
+      console.error("rejected", action.payload);
+    });
+
+    builder.addCase(acceptQuest.fulfilled, (state, action: PayloadAction<QuestResult>) => {
+      state.game.gold = action.payload.gold;
+      state.game.highScore = action.payload.gold;
+      state.game.score = action.payload.score;
+      // state.game.level = action.payload.level;
+      state.game.lives = action.payload.lives;
+      state.game.turn = action.payload.turn;
+      // TODO check action.payload.success
+      // TODO trigger quest list refresh
+    });
+    builder.addCase(acceptQuest.pending, (_state) => {
       
     });
-    builder.addCase(startNewGame.rejected, (state, _action) => {
-      state.started = false;
+    builder.addCase(acceptQuest.rejected, (_state, action) => {
+      console.error("rejected", action.payload);
     });
   }
 });
 
 export const startNewGame = createAsyncThunk(
-  'startNewGame',
-  async () => fetch("https://dragonsofmugloar.com/api/v2/game/start", {method: "POST"})
-  .then(response => response.json())
-  .then(data => data as StartGameResponse)  
+    'startNewGame',
+    async () => fetch("https://dragonsofmugloar.com/api/v2/game/start", {method: "POST"})
+    .then(response => response.json())
+    .then(data => data as StartGameResponse)
+);
+
+export const acceptQuest = createAsyncThunk(
+    'acceptQuest',
+    async (adId: string, {getState}) =>
+        fetch(`https://dragonsofmugloar.com/api/v2/${(getState() as RootState).gameState.game.gameId}/solve/${adId}`, {method: 'POST'})
+        .then(response => response.json())
+        .then(data => data as QuestResult)
 );
