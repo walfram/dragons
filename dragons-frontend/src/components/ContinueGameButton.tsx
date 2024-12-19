637c43b4bee96c379b123f0cab44d5f6a4382a57
Jevgeni Kalinin <jevgeni.kalinin@gmail.com>
2024-12-19 10:32:11 +0200

    added start new game and continue game fetch logic

--- dragons-frontend/src/components/ContinueGameButton.tsx
+++ dragons-frontend/src/components/ContinueGameButton.tsx
@@ -1,20 +1,32 @@
 import {GameId} from "../etc/types.ts";
 import {useAppDispatch} from "../store/store.ts";
 import {hideSpinner, showSpinner} from "../store/spinnerSlice.ts";
-import {delay} from "../etc/debug.ts";
 
-export default function ContinueGameButton({gameId} : {gameId: GameId}) {
+function checkGameIsValid(gameId: GameId): Promise<Response> {
+  return fetch(`https://dragonsofmugloar.com/api/v2/${gameId.gameId}/investigate/reputation`, {method: "post"})
+  .then(response => {
+    console.log("response.ok", response.ok);
+    console.log("response.status", response.status);
+    return response;
+  });
+}
+
+export default function ContinueGameButton({gameId}: { gameId: GameId }) {
   const dispatch = useAppDispatch();
-  
-  async function onClick() {
+
+  function onClick() {
     console.log("continue game", gameId);
     dispatch(showSpinner());
 
-    await delay(3000);
-
-    dispatch(hideSpinner());
+    checkGameIsValid(gameId)
+    .then(response => {
+      console.log("game is valid");
+      return response.json();
+    })
+    .catch(error => console.error("invalid game id", error))
+    .finally(() => dispatch(hideSpinner()))
   }
-  
+
   return (
       <button onClick={() => onClick()}>continue game {gameId.gameId}</button>
   );
--- dragons-frontend/src/components/StartNewGameButton.tsx
+++ dragons-frontend/src/components/StartNewGameButton.tsx
@@ -1,17 +1,33 @@
-import {delay} from "../etc/debug.ts";
 import {useAppDispatch} from "../store/store.ts";
 import {hideSpinner, showSpinner} from "../store/spinnerSlice.ts";
+import {StartGameResponse} from "../etc/types.ts";
+
+function startNewGame() {
+  return fetch(`https://dragonsofmugloar.com/api/v2/game/start`, {method: "post"})
+  .then(response => {
+    console.log("start new game response.ok", response.ok);
+    console.log("start new game response.status", response.status);
+    return response;
+  });
+}
 
 export default function StartNewGameButton() {
   const dispatch = useAppDispatch();
-  
-  async function onClick() {
+
+  function onClick() {
     console.log("starting new game");
     dispatch(showSpinner());
-    await delay(3000);
-    dispatch(hideSpinner());
+
+    startNewGame()
+    .then(response => response.json())
+    .then(data => {
+      const startGameResponse = data as StartGameResponse;
+      console.log("started new game", startGameResponse.gameId)
+    })
+    .catch(error => console.error("cannot start new game", error))
+    .finally(() => dispatch(hideSpinner()));
   }
-  
+
   return (
       <button onClick={() => onClick()}>start new game</button>
   )
