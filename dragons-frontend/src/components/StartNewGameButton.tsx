df1f18379dec7b01b52a10917ce720fc2915d7ac
Jevgeni Kalinin <jevgeni.kalinin@gmail.com>
2024-12-19 10:01:48 +0200

    added spinner to start new game button

--- dragons-frontend/src/components/StartNewGameButton.tsx
+++ dragons-frontend/src/components/StartNewGameButton.tsx
@@ -1,6 +1,15 @@
+import {delay} from "../etc/debug.ts";
+import {useAppDispatch} from "../store/store.ts";
+import {hideSpinner, showSpinner} from "../store/spinnerSlice.ts";
+
 export default function StartNewGameButton() {
-  function onClick() {
+  const dispatch = useAppDispatch();
+  
+  async function onClick() {
     console.log("starting new game");
+    dispatch(showSpinner());
+    await delay(3000);
+    dispatch(hideSpinner());
   }
   
   return (
