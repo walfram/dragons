df6d2c7edfbee1073e46aa9e4ca27a5afbea4a46
Jevgeni Kalinin <jevgeni.kalinin@gmail.com>
2025-01-03 18:19:41 +0200

    added spinner on start new game button click

--- dragons-frontend/src/widgets/select-game/NewGameButton.tsx
+++ dragons-frontend/src/widgets/select-game/NewGameButton.tsx
@@ -1,18 +1,21 @@
 import {useAppDispatch} from "../../store/store.ts";
 import {fetchQuests, startGame} from "../../store/gameInstanceSlice.ts";
+import {hideSpinner, showSpinner} from "../../store/spinnerSlice.ts";
 
 export default function NewGameButton() {
   const dispatch = useAppDispatch();
 
   function onStartNewGameClick() {
     console.log("onStartNewGameClick");
+    dispatch(showSpinner());
     dispatch(startGame())
     .unwrap()
     .then(gameStartResponse => {
       window.history.replaceState(null, "", `?gameId=${gameStartResponse.gameId}`);
       return gameStartResponse;
     })
-    .then(gameStartResponse => dispatch(fetchQuests(gameStartResponse.gameId!)));
+    .then(gameStartResponse => dispatch(fetchQuests(gameStartResponse.gameId!)))
+    .finally(() => dispatch(hideSpinner()));
   }
 
   return (
