3e2f478337e2cf680f3e6e51f6c7de830c8ab4e5
Jevgeni Kalinin <jevgeni.kalinin@gmail.com>
2025-01-05 16:57:38 +0200

    made typo returning decoded quest id

--- dragons-frontend/src/etc/decode.ts
+++ dragons-frontend/src/etc/decode.ts
@@ -12,7 +12,7 @@ export function decodeProbability(quest: Quest) {
   switch (quest.encrypted) {
     case 1 : return atob(quest.probability);
     case 2 : return rot13(quest.probability);
-    default: return quest.probability;
+    default: return quest.adId;
   }
 }
 
