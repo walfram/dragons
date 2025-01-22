ac97a144d19c6ce85421989a45123340ff3be17f
Jevgeni Kalinin <jevgeni.kalinin@gmail.com>
2025-01-22 13:20:03 +0200

    removed saved games todo

--- dragons-frontend/src/views/SelectGameView.tsx
+++ dragons-frontend/src/views/SelectGameView.tsx
@@ -9,11 +9,11 @@ export default function SelectGameView() {
         <NewGameButton />
       </div>
       
-      <div>
-        <p>TODO saved games...?</p>
-        <p>there is no endpoint for "get current game state by game id". saved game can be only "fetched" after quest accept and item purchase</p>
-        <p>at the moment only reasonable way to handle saved game is to persist (localStorage or smth) game state on quest/purchase</p>
-      </div>
+      {/*<div>*/}
+      {/*  <p>TODO saved games...?</p>*/}
+      {/*  <p>there is no endpoint for "get current game state by game id". saved game can be only "fetched" after quest accept and item purchase</p>*/}
+      {/*  <p>at the moment only reasonable way to handle saved game is to persist (localStorage or smth) game state on quest/purchase</p>*/}
+      {/*</div>*/}
       
     </section>  
   );
